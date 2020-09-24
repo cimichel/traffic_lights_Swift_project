4bf479902a15256f18f65a281dcc809860566f35
Cintia <42854408+cimichel@users.noreply.github.com>
2020-09-24 13:31:51 +1000

    Add files via upload

---
+++ transitLights/transitLightsTests/transitLightsTests.swift
@@ -0,0 +1,34 @@
+//
+//  transitLightsTests.swift
+//  transitLightsTests
+//
+//  Created by Cintia Michel on 9/8/20.
+//  Copyright © 2020 Cintia Michel. All rights reserved.
+//
+
+import XCTest
+@testable import transitLights
+
+class transitLightsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
